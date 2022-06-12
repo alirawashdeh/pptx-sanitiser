1926d7da6da9a3f5b2211107132a3e4b314ceb78
Ali Rawashdeh <aimrawashdeh@gmail.com>
2022-06-12 22:47:39 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
+using A = DocumentFormat.OpenXml.Drawing;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            String[] filenames = System.IO.Directory.GetFiles(System.IO.Directory.GetCurrentDirectory(),"*.pptx",SearchOption.AllDirectories);
+            
+            // Process each file
+            bool filesProcessed = false;
+            for (int i = 0; i < filenames.Length; i++)
+            {
+                if(filenames[i].Contains("~$") == false)
+                {
+                    UpdateSlideText(filenames[i]);
+                    filesProcessed = true;
+                    Console.Out.WriteLine("Updated file: " + filenames[i] + " 👌");
+                }
+            }
+
+            if (!filesProcessed)
+            {
+                throw new ArgumentException("No pptx files found.");
+            }
+
+        }
+
+    public static void UpdateSlideText(string docName)
+    {
+        using (PresentationDocument ppt = PresentationDocument.Open(docName, true))
+        {
+            // Get the relationship ID of the first slide.
+            if((ppt.PresentationPart != null) && (ppt.PresentationPart.Presentation.SlideIdList != null))
+            {
+                PresentationPart part = ppt.PresentationPart;
+                OpenXmlElementList slideIds = part.Presentation.SlideIdList.ChildElements;
+
+                // Update document properties
+                ppt.PackageProperties.Creator = null;
+                ppt.PackageProperties.Description = null;
+                ppt.PackageProperties.Subject = null;
+                ppt.PackageProperties.Category = null;
+                ppt.PackageProperties.Keywords = null;
+                ppt.PackageProperties.Title = null;
+                ppt.PackageProperties.LastModifiedBy = ObfuscateText(ppt.PackageProperties.LastModifiedBy);
+                if(ppt.ExtendedFilePropertiesPart != null) 
+                {
+                    ppt.ExtendedFilePropertiesPart.Properties = new DocumentFormat.OpenXml.ExtendedProperties.Properties();
+                }
+
+                // Update the text on each slide
+                for (int i=0; i<part.SlideParts.Count(); i++)
+                {
+                    String relId = String.Empty;
+                    SlideId slideId = (SlideId)slideIds[i];
+                    if(slideId.RelationshipId != null)
+                    {
+                        relId = slideId.RelationshipId;
+                    }
+
+                    // Get the slide part from the relationship ID.
+                    SlidePart slide = (SlidePart) part.GetPartById(relId);
+
+                    // Get the inner text of the slide:
+                    IEnumerable<A.Text> texts = slide.Slide.Descendants<A.Text>();
+                    
+                    foreach (A.Text text in texts)
+                    {
+                        Regex pattern = new Regex("[A-Za-z0-9]");
+                        text.Text = ObfuscateText(text.Text);
+                    }
+                }
+            }
+        }              
+    }   
+
+    public static String ObfuscateText(string text)
+    {
+        Regex pattern = new Regex("[A-Za-z0-9]");
+        return pattern.Replace(text, "x");
+    }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# pptx-sanitiser
-Quickly obfuscate text in powerpoint files
+# Powerpoint Sanitiser
+
+Quickly and easily obfuscate the text in one or more Powerpoint files (PPTX only). Useful when sharing slide template layouts with others without sharing the content.
+
+![Diagram](/screenshot.png)
+
+# Installation
+
+Ensure that .Net Core 3.1 is installed, then carry out the following steps:
+
+```
+git clone https://github.com/alirawashdeh/pptx-sanitiser.git
+cd pptx-sanitiser
+dotnet restore
+```
+
+# Usage
+
+Place pptx files into the ```pptx-sanitiser``` folder or any subfolder. There is an example file in the ```pptx-sanitiser/pptx``` folder.
+Run the application using the following:
+
+```
+dotnet run
+```
+
+A message will be outputted for each file processed:
+
+```
+Updated file: /Users/person/git/pptx-sanitiser/pptx/example.pptx 👌
+```
+
+## Warnings
+
+- :warning: **Files are updated in situ** so make sure you copy the ```pptx``` files into the ```pptx-sanitiser folder``` rather than moving them
+
+- :warning: Images are not sanitised during this process so make sure that you check each processed file for images (in the slides or master)
+
+- :warning: Text within SmartArt
